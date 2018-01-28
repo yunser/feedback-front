6e3d79f9e7bdb1f9f41d36c098c9724aca11c98f
cjh <1418503647@qq.com>
2018-01-29 00:05:46 +0800

    update ui

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,7 @@
-import header from './header'
-import footer from './footer'
 import page from './page'
 
 export default {
     install: function (Vue) {
-        Vue.component('ui-header', header)
-        Vue.component('ui-footer', footer)
         Vue.component('my-page', page)
     }
 }
